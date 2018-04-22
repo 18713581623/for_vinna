9b4c7fd813aef40fe7662a2777654e8080132688
vinna <631371776@qq.com>
2018-04-22 17:48:18 +0800

    添加vinna3

---
+++ vinna3/vinna3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vinna3.MvcApplication" Language="C#" %>
---
+++ vinna3/vinna3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vinna3.Startup))]
+namespace vinna3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
